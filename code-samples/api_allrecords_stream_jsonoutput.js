90c4c35d4ed65387492219e546c9dc731de23669
Kayla Vanderlyn <kayla.vanderlyn@associates.fema.dhs.gov>
2024-07-11 09:28:17 -0400

    feat: :sparkles: make streaming data code sample overwrite out file instead of appending to it

--- code-samples/api_allrecords_stream_jsonoutput.js
+++ code-samples/api_allrecords_stream_jsonoutput.js
@@ -15,7 +15,7 @@ format = "&$format=json"; // set the format to json, json is the default format
 other = "&$allrecords=true&$metadata=off"; // return all records, suppress metadata
 
 let jsonFile = "./out.json"; // set the name of the output file
-const  writeStream = fs.createWriteStream(jsonFile, {flags:'a'}); // set up the file to receive streaming data
+const  writeStream = fs.createWriteStream(jsonFile); // set up the file to receive streaming data, if file exists, overwrites it
 
 // async function handles all data manipulation
 const saveStreamingData = async () => {
